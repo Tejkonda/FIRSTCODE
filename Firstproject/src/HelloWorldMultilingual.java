a676e30f626f02f6b0d196952180015a340ace8f
Tejkonda <131727180+Tejkonda@users.noreply.github.com>
2023-05-03 13:49:46 -0700

    FIrstProject

---
+++ Firstproject/src/HelloWorldMultilingual.java
@@ -0,0 +1,13 @@
+public class HelloWorldMultilingual {
+    public static void main(String[] args){
+        //prints "Hello World!" languages.
+        // Tamil
+        System.out.println("ஹலோ உலகம்!");
+        // Tatar
+        System.out.println("Сәлам, Дөнья!");
+        // Telugu
+        System.out.println("హలో వరల్డ్!");
+        // Thai
+        System.out.println("สวัสดีโลก!");
+    }
+}
---
+++ Firstproject/src/Initials.java
@@ -0,0 +1,14 @@
+public class Initials {
+    public static void main(String[] args){
+        //prints initial using nine rows of asterisks.
+        System.out.println("**        ***    **********      **             *             **");
+        System.out.println("**      ***      **        **     **           ***           ** ");
+        System.out.println("**    ***        **         **     **         ** **         **  ");
+        System.out.println("**  ***          **          **     **       **   **       **   ");
+        System.out.println("*****            **          **      **     **     **     **    ");
+        System.out.println("**  ***          **          **       **   **       **   **     ");
+        System.out.println("**    ***        **         **         ** **         ** **      ");
+        System.out.println("**      ***      **        **           ***           ***       ");
+        System.out.println("**        ***    **********              *             *        ");
+    }
+}
